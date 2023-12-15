01438e6c95b315daf1eee0ac46141f368336acfa
y yd <2646155814@qq.com>
2023-12-15 21:50:52 +0800

    continue menu

--- js/src/menu/zbase.js
+++ js/src/menu/zbase.js
@@ -2,9 +2,11 @@ class Menu {
     constructor(root) {
         this.root = root;
         this.$menu = $(`
-            <div style='height:100vh;width:100vw'>
+            <div class='background'>
                 <div class='menu'>
                     <div class='input_and_random'>
+                        <div class="input_form col-md-4 mx-auto">
+                        </div>
                         <div class="select_by_input d-grid gap-2.5 col-6 mx-auto">
                             <button class="select_by_input_button btn btn-primary" type="button">自己输入</button>
                         </div>
@@ -28,28 +30,37 @@ class Menu {
                     </div>
                     <div class='submit_button'>
                         <div class="d-grid gap-2.5 col-6 mx-auto">
-                            <button class="select_by_random btn btn-primary" type="button">提交</button>
+                            <button class="submit_button btn btn-primary" type="button">提交</button>
                         </div>
                     </div>
                 </div>
             </div>
         `)
 
-        this.$input_box = $(`
-            <div class="input_box">
-                <div class="form-floating">
-                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-                    <label for="floatingTextarea">Comments</label>
-                </div>
+        this.$input_group = $(`
+            <div class="input-group">
+                <span class="input-group-text" id="inputGroup-sizing-default">Process Info</span>
+                <input type="text" placeholder="ProcessName" class="form-control">
+                <input type="text" placeholder="ArrivalTime" class="form-control">
+                <input type="text" placeholder="Priority" class="form-control">
+                <input type="text" placeholder="BurstTime" class="form-control">
+                <button type="button" class="btn btn-danger">delete</button>
             </div>
         `)
 
+
         // this.$menu.hide();
         this.$select_by_input = this.$menu.find('.select_by_input');
+        this.$select_by_random = this.$menu.find('.select_by_random');
         this.$select_by_input_button = this.$menu.find('.select_by_input_button');
+        this.$select_by_random_button = this.$menu.find('.select_by_random_button');
+        this.$input_form = this.$menu.find('.input_form');
         this.$submit_button = this.$menu.find('.submit_button');
 
+        this.$input_form.hide();
+
         this.root.$schedule.append(this.$menu);
+        this.$input_form.append(this.$input_group);
         
         this.start();
     }
@@ -60,8 +71,23 @@ class Menu {
     
     add_listen_events() {
         let outer = this;
-        outer.$select_by_input_button.click(function() {
+
+        this.$select_by_input_button.click(() => {
             outer.$select_by_input_button.hide();
+            outer.$select_by_random_button.show();
+            outer.$input_form.show();
+        });
+
+        this.$select_by_random_button.click(() => {
+            outer.$select_by_random_button.hide();
+            outer.$select_by_input_button.show();
+            outer.$input_form.hide();
+        });
+
+        this.$input_group.on('keyup','input-group:child',(e) => {
+            if(e.key == 'Enter') {
+                $this.$input_form.append(this.$input_group.clone());
+            }
         });
     }
 }
